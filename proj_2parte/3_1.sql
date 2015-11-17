77ade64800695ec576b4e450255934c78ffcefb5
bernardo5 <bdnabaisgomes@yahoo.com>
2015-11-17 22:43:22 +0000

    1 querie a toa

--- proj_2parte/3_1.sql
+++ proj_2parte/3_1.sql
@@ -1,3 +1,7 @@
+SELECT C.snum, C.manuf, C.pan FROM Connects as C WHERE(
+	(C.pan IN (SELECT Wears.pan FROM Wears
+					WHERE (Wears.end>=DATE_SUB(CURDATE(), INTERVAL  6 MONTH))
+							AND('001-54245-1555555'=Wears.patient))) AND
+	(C.end>=DATE_SUB(CURDATE(), INTERVAL  6 MONTH))
 
-	SELECT Wears.pan, Wears.start, Wears.end FROM Wears
-	WHERE (Wears.end>=DATE_SUB(CURDATE(), INTERVAL  6 MONTH))AND('001-54245-1555555'=Wears.patient);
+);
