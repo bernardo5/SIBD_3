8c689fbb3a43e26722d81e5a12bf45c065cd360b
bernardo5 <bdnabaisgomes@yahoo.com>
2015-11-25 18:09:11 +0000

    relatorio com triggers

---
+++ proj_2parte/report/zip/trigger_pan_device_update.sql
@@ -0,0 +1,26 @@
+drop trigger if exists check_overlap_time_period_Device_PAN_update;
+
+
+delimiter $$
+
+create trigger check_overlap_time_period_Device_PAN_update before update on Connects
+
+for each row
+begin
+if(
+	exists(SELECT start, end FROM Connects
+		WHERE(
+			(Connects.start <= new.end) and (Connects.end >= new.start) 	/*Condição de Sobreposição*/ 
+			AND ((new.snum = Connects.snum) and (new.manuf = Connects.manuf))
+			AND (new.pan != Connects.pan)	
+		)
+	)
+)
+	then	
+	call overlaping_time_periods_for_Device_PAN();
+
+end if;
+
+end$$
+
+delimiter ;
